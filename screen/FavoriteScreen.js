cd504869e8b08262002aff111f900e1862b4b720
in-kim <ehddls960617@naver.com>
2023-06-17 12:18:44 +0900

    Update. 예외처리

--- screen/FavoriteScreen.js
+++ screen/FavoriteScreen.js
@@ -1,12 +1,34 @@
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 import MealList from "../components/MealList/List";
 import {FavoritesContext} from "../store/context";
 import {useContext} from "react";
 import {MEALS} from "../data/dummy-data";
 export default function FavoriteScreen(){
   const favoriteContext = useContext(FavoritesContext);
-  const favoriteList = MEALS.filter(mealItem => {
+  const favoriteMeals = MEALS.filter(mealItem => {
     return favoriteContext.isFavoriteId(mealItem.id)
   });
-  return (<MealList mealList={favoriteList}/>)
+
+  console.log(favoriteMeals)
+  if(!favoriteMeals.length > 0){
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meal yet.</Text>
+      </View>
+    )
+  }
+
+  return (<MealList mealList={favoriteMeals}/>)
 }
+
+const styles = StyleSheet.create({
+  rootContainer:{
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    color:'#fff'
+  }
+})
